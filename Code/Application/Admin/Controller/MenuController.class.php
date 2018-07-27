feae57c1c6957085ee0eb24f823da3535dda17de
346781123 <346781123@qq.com>
2018-07-27 16:20:47 +0800

    submit

--- Code/Application/Admin/Controller/MenuController.class.php
+++ Code/Application/Admin/Controller/MenuController.class.php
@@ -15,4 +15,52 @@ class MenuController extends Controller {
     public function front(){
         $this->display();
     }
+
+    public function add(){
+        if(IS_AJAX){
+            $model = D('SysMenu');
+            if($model->create()){
+                $ret = $model->add();
+                if($ret){
+                    $return['status'] = 1;
+                    $return['msg'] = '添加成功';
+                }else{
+                    $return['status'] = 0;
+                    $return['msg'] = '添加失败';
+                }
+            }
+            $this->ajaxReturn($return);
+        }else{
+            $parent_id = I('parent_id');
+            if(!empty($parent_id)){
+                $this->assign('parent_id',$parent_id);
+            }
+            $this->assign('menu',D('SysMenu')->getAllMenu());
+            $this->display();
+        }
+    }
+
+    public function edit(){
+        if(IS_AJAX){
+            $model = D('SysMenu');
+            if($model->create()){
+                $ret = $model->add();
+                if($ret){
+                    $return['status'] = 1;
+                    $return['msg'] = '添加成功';
+                }else{
+                    $return['status'] = 0;
+                    $return['msg'] = '添加失败';
+                }
+            }
+            $this->ajaxReturn($return);
+        }else{
+            $id = I('id');
+            if(!empty($id)){
+                $this->assign('menu',D('SysMenu')->find($id));
+            }
+            $this->assign('menu',D('SysMenu')->getAllMenu());
+            $this->display();
+        }
+    }
 }
