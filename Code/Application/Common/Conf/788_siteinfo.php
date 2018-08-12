65aa6c5bf23857f2603759aeeeef9f319bd8ee74
tornado <346781123@qq.com>
2018-08-13 00:09:34 +0800

    init

--- Code/Application/Common/Conf/788_siteinfo.php
+++ Code/Application/Common/Conf/788_siteinfo.php
@@ -2,11 +2,11 @@
 return array(
 	//'配置项'=>'配置值'
     'SITE_INFO' => array(
+        'TITLE' => '788无极软件',
         'SITE_NAME' => '无极软件后台管理系统',
         'QQ' => '6410788',
         'EMAIL' => '6410788@qq.com',
         'SERVER_TEL' => '18784599969',
-        'EMAIL' => '6410788@qq.com',
         'COMPANY' => '乐山七八八网络科技有限公司',
         'COPY_RIGHT' => "蜀ICP备18019103号-1",
     ),
